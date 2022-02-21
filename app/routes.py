183841d3b45e5bbdb27b99d30d7547005e81637f
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-02-21 17:38:10 +0000

    c

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,21 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'BaysahGuwor'}
-    return render_template('index.html', user=user)
+    user = {'username': 'Baysah Guwor'}
+    posts = [
+        { 
+            'author': {'username':'Yamah Kerkulah'},
+            'body': 'I really like making app'
+        },
+        {
+            'author':{'username':'Flomo Johnson'},
+            'body': 'My dog is really liking milk.'
+        },
+        {
+            'author':{'username':'Access Guwor'},
+            'body':'I love reading and writing, it makes me happy!'
+        }
+
+            ]
+
+    return render_template('index.html', user=user, posts=posts )
