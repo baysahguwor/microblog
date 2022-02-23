2db875c683f4fb6783a50829862d5a8b0c6a679b
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-02-23 17:32:43 +0000

    change

--- app/config.py
+++ app/config.py
@@ -1,4 +1,16 @@
+
+
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or '79537d00f4834892986f09a100aa1edf'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 from distutils.log import Log
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
-@app.route('/login')
+app.config["SECRET_KEY"] = '79537d00f4834892986f09a100aa1edf'
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
 
     return render_template('login.html', title='Sign In', form=form)
     
