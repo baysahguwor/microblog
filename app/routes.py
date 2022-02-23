d0daae28eb1c42da7c3604fce04ff6863c4f2ab5
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-02-23 11:34:54 +0000

    change

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from distutils.log import Log
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html', title='Sign In', form=form)
+    
 @app.route('/')
 @app.route('/index')
 def index():
