cfbab9cf8a949fe08a654176ddb597b434c0c3e5
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-03-12 22:42:32 +0000

    Major change 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login = LoginManager(app)
+login.login_view = 'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models 
+from app import routes, models
