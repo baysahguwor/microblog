1a5b29e0b048f838460721dce2fc46e517ec2490
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-03-23 15:09:16 +0000

    Chapter 13
    support multiple languages

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or '79537d00f4834892986f09a100aa1edf'
-
-import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -12,9 +8,10 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('587') or 25)
+    MAIL_PORT = int(os.environ.get('487') or 25)
     MAIL_USE_TLS = os.environ.get('1') is not None
     MAIL_USERNAME = os.environ.get('guworbaysah@gmail.com')
     MAIL_PASSWORD = os.environ.get('Baysah369109')
-    ADMINS = ['guworbaysah@gmail.com']
-    POSTS_PER_PAGE = 5
+    ADMINS = ['itcgbarnga@gmail.com']
+    LANGUAGES = ['en', 'es']
+    POSTS_PER_PAGE = 25
