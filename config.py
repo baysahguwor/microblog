c2b592a3fdf924a64dc904cd6f92f9d313169e9f
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-03-29 15:17:08 +0000

    Heroku db creation

--- config.py
+++ config.py
@@ -7,14 +7,15 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+      #.replace(
+      #  'postgres://', 'postgresql://') or \
+       # 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_SERVER = os.environ.get('smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('587') or 25)
+    MAIL_USE_TLS = os.environ.get('True') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['guworbaysah@gmail.com']
