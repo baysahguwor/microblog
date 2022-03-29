69f57aead9d263e024cbccf9960e99e3e18b6283
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-03-29 16:26:14 +0000

    test3

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #.replace(
+      #  'postgres://', 'postgresql://') or \
+      #  'sqlite:///' + os.path.join(basedir, 'app.db')
+   # SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
