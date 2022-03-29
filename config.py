d8efb2c9aa920b8b2f0d2d776c8059eb9539327f
Baysah Guwor <92314340+baysahguwor@users.noreply.github.com>
2022-03-29 15:21:48 +0000

    Huroku Db changes

--- config.py
+++ config.py
@@ -7,17 +7,16 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-      #.replace(
-      #  'postgres://', 'postgresql://') or \
-       # 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('587') or 25)
-    MAIL_USE_TLS = os.environ.get('True') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('guworbaysah@gmail.com')
+    MAIL_PASSWORD = os.environ.get('Baysah@369109')
     ADMINS = ['guworbaysah@gmail.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
